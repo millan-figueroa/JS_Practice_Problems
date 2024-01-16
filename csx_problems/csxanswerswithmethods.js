ccb08cf3ea0b80e79169b730da8fbaca5a1be39d
millan <millan.fig@gmail.com>
2024-01-15 23:47:41 -0800

    master

--- csx_problems/csxanswerswithmethods.js
+++ csx_problems/csxanswerswithmethods.js
@@ -14,3 +14,23 @@ function intersection(arrays) {
   const arr2 = [15, 88, 1, 5, 7];
   const arr3 = [1, 10, 15, 5, 20];
   console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
+
+
+  
+  function majority(array,callback) {
+    //using reduce keeps track of the count, adding the empty object as initial value so it returns resultObj
+      const counter = array.reduce((resultObj, current) => {
+        //result is the result of the callback being invoked on each current element
+        const result = callback(current);
+        //checking if current result exists in resultObj, uses its current value; otherwise, it defaults to 0.
+        resultObj[result] = (resultObj[result] || 0) + 1; //increments value by 1
+         return resultObj;//returns resultObj out to main function scope
+      },{});
+      //returns value that is present more times
+      return counter[true] > counter[false];
+    }
+    
+    // Uncomment these to check your work!
+    const isOdd = function(num) { return num % 2 === 1; };
+    console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+    console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
--- csx_problems/objects.js
+++ csx_problems/objects.js
@@ -1,85 +1,118 @@
-    Inventory.prototype.addItem = function(item, price) {
-      if(!this.hasOwnProperty(item)){ //if item does not exist in object, add
-          this[item] = { price : price, quantity : 1 }
-        } else { this[item].price = price, this[item].quantity++ }; //else, update quantity
-    }
-  
-    Inventory.prototype.deleteItem = function(item, price) {
-      if((!this.hasOwnProperty(item)) || (this[item].quantity <= 0)){
-        return 'Nothing to delete';
-      } else {
-        this[item].quantity -= 1;
-        return 'Deleted';
-      };
-    }
-  
-    Inventory.prototype.checkItem = function(item, price) {
-      if(!this.hasOwnProperty(item)){
-        return 'Item is not in inventory';
-      } else {
-        return this[item];
-      };
-    }
+/*Challenge: Inventory
+Declare a function Inventory that, when invoked with the new keyword, returns objects that keep track of an inventory. Your Inventory function should accept two arguments: item, a string, and price, a number. The object returned should contain a property whose key matches the passed-in item argument, and is set to another object with two properties: price (which will be set to the price argument the method was called with) and quantity, which should be initially set to 1.
+
+Each object returned from this Inventory constructor should, via the prototype chain, have access to three methods:
+
+The method addItem will add additional items to the object returned from the constructor. It should also accept an item and price argument, and when invoked, should check to see if the Inventory object it is called upon has a property that matches the passed-in item name. If it does not, add one, and set it to another item object, following the same format as above. If the Inventory object does have a property with that name, increment that item's quantity property by one, and replace the price with whatever number was just passed in.
+
+The method deleteItem should accept a string as an argument. If a matching property exists on the Inventory object, decrement its quantity by 1 and return the string 'Deleted'. If the quantity is already 0, return the string 'Nothing to delete'.
+
+The method checkItem should take a string as an argument and check to see whether that string exists as a property on the Inventory object. If so, return the object for that item. If not, return the string 'Item is not in inventory'.
+
+ */
 
 const myInventory = new Inventory('cucumber', 2);
 
 // Uncomment these lines to check your work!
-myInventory.addItem('carrot', 1);
-console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
-myInventory.addItem('cucumber', 5); 
-console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
-console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
-console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
-console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
+// myInventory.addItem('carrot', 1);
+// console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
+// myInventory.addItem('cucumber', 3); 
+// console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
+// console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
+// console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
+// console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
+/* Challenge: Dog Constructor
+Declare a function Dog that creates a new instance of a Dog object when invoked with the new keyword. Each Dog object should have a name property and a breed property, both strings which are passed in as arguments when calling the Dog function. It should also have a property called tricks, set to an array representing all the tricks that dog knows. When a new object is initiated, tricks should be empty.
 
-/* In the openStore method, you are overwriting the this.owner property each time a new store is 
-opened. It would be better to store owners in an array or object to keep track of multiple owners 
-for different stores.
+All of your Dog objects must also have access to two methods that are stored on the constructor's 
+prototype:
 
-In the closeStore method, you are using delete this.city, which is incorrect. Instead, you should 
-find the index of the city in the this.locations array and remove it.*/
+The first method, learnTrick, should take in a string as an argument and add that string to the 
+tricks array of the particular Dog object it was called on.
 
-// class Chain {
-// 	constructor(name){
-//         this.name = name;
-//         this.totalStores = 0;
-//         this.locations = [];
-//     }
-    
-//         openStore(owner,city){
-//             const newFran = new Franchise(owner,city);
-//             // this.owner = owner;
-//             this.locations.push(newFran);
-//             this.totalStores++;
-//         };
-//         closeStore(city) {
-//             for (let i = 0; i < this.locations.length; i++) {
-//                 if (this.locations[i].city === city) {
-//                     this.locations.splice(i, 1);
-//                     this.totalStores--;
-//                     console.log(`${this.name} closed the store in ${city}!`);
-//                     // return; // we return so the default statement is not printed
-//                 }
-//             } 
-//       console.log(`${this.name} doesn't have a store in ${city}.`);
-//     }
-// }
-// class Franchise {
-//     constructor(owner,city){
-//         this.owner = owner;
-//         this.city = city;
-//     }
-// }
+The second method, performTrick, should also take a string as an argument. It should check to see 
+if that string is in the tricks array belonging to the Dog instance it was called on; if so, it 
+should log the string 'name performed trick!' If not, log the string 'name doesn't know that trick.*/
 
-// const buenoBell = new Chain('Bueno Bell');
+function Dog(name, breed) {
+	// add code here
 
-// // Uncomment these lines to check your work!
-// buenoBell.openStore('Will', 'Phoenix');
-// buenoBell.openStore('Kyle', 'Austin');
-// buenoBell.openStore('Allison', 'Wichita');
+}
 
-// console.log(buenoBell.totalStores); //should log 3
-// console.log(buenoBell.locations[0].owner) //should log 'Will'
+const fido = new Dog('Fido', 'poodle');
 
-// buenoBell.closeStore('Austin'); // Should log 'Bueno Bell closed the store in Austin.'
-// buenoBell.closeStore('Maui'); // Should log 'Bueno Bell doesn't have a store in Maui.'
+// Uncomment these lines to check your work!
+// fido.learnTrick('fetch');
+// fido.performTrick('fetch'); // should log 'Fido performed fetch!'
+// fido.performTrick('sit'); // should log 'Fido doesn't know that trick.'
+
+
+/* Challenge: PersonConstructor
+Create a function PersonConstructor that uses the this keyword to save a single property onto its 
+scope called greet. greet should be a function that logs the string "hello". */
+
+function PersonConstructor() {
+	this.greet = function(){console.log('hello')}
+}
+
+const simon = new PersonConstructor();
+
+// Uncomment this line to check your work!
+simon.greet(); // -> Logs 'hello'
+
+/*: personFromConstructor
+Create a function personFromConstructor that takes as input a name and an age. When called, the 
+function will create person objects using the new keyword instead of the Object.create method.*/
+
+
+const mike = personFromConstructor('Mike', 30);
+// Uncomment these lines to check your work!
+console.log(mike.name); // -> Logs 'Mike'
+console.log(mike.age); // -> Logs 30
+mike.greet(); // -> Logs 'hello'
+
+
+/* Challenge: introduce (cont.)
+Without editing the code you've already written, add an introduce method to the PersonConstructor 
+function that logs "Hi, my name is [name]".*/
+
+const mike = personFromConstructor('Mike', 30);
+
+// add code here
+
+
+// Uncomment this line to check your work!
+// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+
+/*Challenge: Chain Stores
+Declare a class Chain that creates a new instance of a Chain object when invoked with the new keyword. Each Chain object should have a name property, a totalStores property, and a locations property. The name property will be assigned to a string which is passed in as an argument when calling the Chain function. The totalStores property should be initialized to 0 and the locations property set to an array representing all the existing franchises of that chain store. When a new object is initiated, the locations array should be empty.
+
+Declare another class Franchise that creates a new instance of a Franchise object when invoked with the new keyword. Each Franchise object should have an owner property and a city property, both of which will be set equal to strings passed in as arguments to the constructor.
+
+All instances of your Chain class must also have access to two methods that are stored on the class's prototype:
+
+The first method, openStore, should take in 2 strings as arguments. Those strings should be passed into a new instance of a Franchise object as the arguments for the owner and city properties. The new instance of Franchise should be added to the locations array which belongs to the Chain instance the method was called on. The totalStores property should also be updated accordingly.
+
+The second method, closeStore, should take a single string as an argument. It should check to see if there is an instance of a Franchise with a city property that matches the input string in the locations array on the instance of Chain the method was called on. If so, it should remove the instance from the locations array, update the totalStores property and display the string 'name closed the store in city!' If not, log the string 'name doesn't have a store in city.' */
+
+class Chain {
+	// add code here
+}
+
+class Franchise {
+    //add code here
+}
+
+
+const buenoBell = new Chain('Bueno Bell');
+
+// Uncomment these lines to check your work!
+//buenoBell.openStore('Will', 'Phoenix');
+//buenoBell.openStore('Kyle', 'Austin');
+//buenoBell.openStore('Allison', 'Wichita');
+
+//console.log(buenoBell.totalStores); //should log 3
+//console.log(buenoBell.locations[0].owner) //should log 'Will'
+
+//buenoBell.closeStore('Austin'); // Should log 'Bueno Bell closed the store in Austin.'
+//buenoBell.closeStore('Maui'); // Should log 'Bueno Bell doesn't have a store in Maui.'
