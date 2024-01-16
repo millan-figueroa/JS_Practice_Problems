1ceb2b7119279893f9c4685ca10ca1af7b679f9d
millan-figueroa <millan.fig@gmail.com>
2024-01-16 00:16:12 -0800

    Added rest of callback and closures problems, also saved all problems solved with methods.

--- csx_problems/callbacks.js
+++ csx_problems/callbacks.js
@@ -118,4 +118,112 @@ function repeater(str) { return str + str; }
 const items = ['catfood', 'glue', 'beer'];
 const functions = [uppercaser, capitalize, repeater];
 
-console.log(multiMap(items, functions)); // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+console.log(multiMap(items, functions)); 
+// should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], 
+//glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+// Create a function subtractTwo that accepts a number and returns that number minus 2.
+
+// Then create a function map that takes two inputs -
+
+// an array of numbers (a list of numbers)
+// a 'callback' function - this function is applied to each element of the array (inside of the function 'map')
+// Have your map function return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array. Please do not use the native map or forEach method.
+
+// Uncomment these to check your work!
+// console.log(typeof subtractTwo); // should log: 'function'
+// console.log(typeof map); // should log: 'function'
+// console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
+
+/*Challenge: reduce
+The function reduce takes an array and reduces the elements to a single value.
+
+The reduce function loops through the array and applies any operation that you can put into a function to each element in the array while keeping track of the outcome of each loop. In this way, we could use reduce to do things like sum all the numbers in an array or multiply them all together.*/
+
+// Uncomment these to check your work!
+// const nums = [4, 1, 3];
+// const add = function(a, b) { return a + b; }
+// console.log(reduce(nums, add, 0)); // should log 8
+
+/*Challenge: objOfMatches
+Construct a function objOfMatches that accepts two arrays and a callback. objOfMatches will build an object and return it. To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array. If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value. */
+
+// Uncomment these to check your work!
+// const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
+// const arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
+// function uppercaser(str) { return str.toUpperCase(); }
+// console.log(objOfMatches(arr1, arr2, uppercaser)); // should log: { hi: 'HI', bye: 'BYE', hello: 'HELLO' }
+
+/*Challenge: arrToObj
+Add code to the function arrToObj in the place marked "ADD CODE HERE" in order to achieve the desired console logs. arrToObj should return an object that has elements from the passed-in array as keys, and the outputs from the callback (when those elements are passed in) as the corresponding values. */
+
+// Uncomment these to check your work!
+// const arrOfStrings = ['beer', 'glue'];
+// const capitalize = str => str.toUpperCase();
+// console.log(arrToObj(arrOfStrings, capitalize)); // should log: { beer: 'BEER', glue: 'GLUE' }
+
+/*Challenge: joinAndMap
+Construct a function joinAndMap with three parameters: array1, array2, and a callback function. The function should combine the two arrays then invoke the callback function on each element, returning the resulting array. This function should not mutate the passed-in arrays. */
+
+// Uncomment these to check your work!
+// const arrRed = ['strawberry', 'cherry', 'wine'];
+// const arrBlue = ['blueberry', 'sky', 'ocean'];
+// const capitalize = str => str.toUpperCase();
+// console.log(joinAndMap(arrRed, arrBlue, capitalize)); // should log: ['STRAWBERRY', 'CHERRY', 'WINE', 'BLUEBERRY', 'SKY', 'OCEAN']
+
+/*Challenge: multiMap
+Construct a function multiMap that will accept two arrays - an array of values and an array of callbacks. multiMap will return an object whose keys match the elements in the array of values. The corresponding values that are assigned to the keys will be arrays consisting of outputs from the array of callbacks, where the input to each callback is the key. */
+
+// Uncomment these to check your work!
+// function uppercaser(str) { return str.toUpperCase(); }
+// function capitalize(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }
+// function repeater(str) { return str + str; }
+// const items = ['catfood', 'glue', 'beer'];
+// const functions = [uppercaser, capitalize, repeater];
+// console.log(multiMap(items, functions)); // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+/*Challenge: majority
+Create a function majority that accepts an array and a callback. The callback will return either true or false. majority will iterate through the array and perform the callback on each element until it can be determined if the majority of the return values from the callback are true. If the number of true returns is equal to the number of false returns, majority should return false. */
+
+// Uncomment these to check your work!
+// const isOdd = function(num) { return num % 2 === 1; };
+// console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+// console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
+
+/*Challenge: prioritize
+Create a function prioritize that accepts an array and a callback. The callback will return either true or false. prioritize will iterate through the array and perform the callback on each element, and return a new array, where all the elements that yielded a return value of true come first in the array, and the rest of the elements come second. */
+
+// Uncomment these to check your work!
+// function startsWithS(str) { return str[0].toLowerCase() === 's'; }
+// const tvShows = ['curb', 'rickandmorty', 'seinfeld', 'sunny', 'friends']
+// console.log(prioritize(tvShows, startsWithS)); // should log: ['seinfeld', 'sunny', 'curb', 'rickandmorty', 'friends']
+
+/*Challenge: countBy
+Create a function countBy that accepts an array and a callback, and returns an object. countBy will iterate through the array and perform the callback on each element. Each return value from the callback will be saved as a key on the object. The value associated with each key will be the number of times that particular return value was returned. */
+
+// Uncomment these to check your work!
+// function evenOdd(n) {
+//   if (n % 2 === 0) return 'even';
+//   else return 'odd';
+// }
+// const nums = [1, 2, 3, 4, 5];
+// console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
+
+/* Challenge: groupBy
+Create a function groupBy that accepts an array and a callback, and returns an object. groupBy will iterate through the array and perform the callback on each element. Each return value from the callback will be saved as a key on the object. The value associated with each key will be an array consisting of all the elements that resulted in that return value when passed into the callback.*/
+
+// Uncomment these to check your work!
+// const decimals = [1.3, 2.1, 2.4];
+// const floored = function(num) { return Math.floor(num); };
+// console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+/*Challenge: goodKeys
+Create a function goodKeys that accepts an object and a callback. The callback will return either true or false. goodKeys will iterate through the object and perform the callback on each value. goodKeys will then return an array consisting only the keys whose associated values yielded a true return value */
+
+// Uncomment these to check your work!
+// const sunny = { mac: 'priest', dennis: 'calculator', charlie: 'birdlaw', dee: 'bird', frank: 'warthog' };
+// function startsWithBird(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
+// console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
+
+
+
--- csx_problems/closures.js
+++ csx_problems/closures.js
@@ -33,3 +33,53 @@ const afterCalled = after(3, addFour)
 console.log(afterCalled(5)); //undefined
 console.log(afterCalled(5)); //undefined
 console.log(afterCalled(5)); //9
+
+/* Create a function saveOutput that accepts a function (that will accept one argument), and a string (that will act as a password). saveOutput will then return a function that behaves exactly like the passed-in function, except for when the password string is passed in as an argument. When this happens, the returned function will return an object with all previously passed-in arguments as keys, and the corresponding outputs as values*/
+
+// Uncomment these to check your work!
+const multiplyBy2 = function(num) { return num * 2; };
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // should log: 4
+console.log(multBy2AndLog(9)); // should log: 18
+console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }
+
+
+/*
+Challenge: cycleIterator
+Create a function cycleIterator that accepts an array, and returns a function. The returned function 
+will accept zero arguments. When first invoked, the returned function will return the first element 
+of the array. When invoked a second time, the returned function will return the second element of 
+the array, and so forth. After returning the last element of the array, the next invocation will 
+return the first element of the array again, and continue on with the second after that, and so 
+forth.*/
+
+// Uncomment these to check your work!
+// const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+// const getDay = cycleIterator(threeDayWeekend);
+
+/*Challenge: dateStamp
+Create a function dateStamp that accepts a function and returns a function. The returned function 
+will accept whatever arguments the passed-in function accepts and return an object with a date key 
+whose value is today's date (not including the time) represented as a human-readable string (see 
+the Date object for conversion methods), and an output key that contains the result from invoking 
+the passed-in function. */
+
+// Uncomment these to check your work!
+// const stampedMultBy2 = dateStamp(n => n * 2);
+// console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+// console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
+
+
+/* Create a function censor that accepts no arguments. censor will return a function that will accept 
+either two strings, or one string. When two strings are given, the returned function will hold onto 
+the two strings as a pair, for future use. When one string is given, the returned function will 
+return the same string, except all instances of a first string (of a saved pair) will be replaced 
+with the second string (of a saved pair).*/
+
+// Uncomment these to check your work!
+// const changeScene = censor();
+// changeScene('dogs', 'cats');
+// changeScene('quick', 'slow');
+// console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); 
+// should log: 'The slow, brown fox jumps over the lazy cats.'
+
